d38eb3a760208aa36e1d41d452a8b9454632d7fc
kaanyigitkocak <kaanyigitkocak@gmail.com>
2023-07-15 20:25:19 +0300

    empty

--- src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -24,7 +24,6 @@ public class EfRepositoryBase<TEntity, TEntityId, TContext> : IAsyncRepository<T
 
     public async Task<TEntity> AddAsync(TEntity entity)
     {
-       
         entity.CreatedDate = DateTime.UtcNow;
         await Context.AddAsync(entity);
         await Context.SaveChangesAsync();
