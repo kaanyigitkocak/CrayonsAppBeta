493a5c7df9cb0a463ecc4a9d840e0d1c2c54d483
kaanyigitkocak <kaanyigitkocak@gmail.com>
2023-08-12 18:13:14 +0300

    Notification

---
+++ src/starterProject/Application/Notifications/Mails/MailNotification.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Auth.Constants;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using MediatR;
+using System.Diagnostics;
+
+namespace Application.Notifications.Mails;
+
+
+public class MailNotification : INotification
+{
+    public MailNotification(string title, string message) 
+    {
+        Title = title;
+        Message = message;
+    }
+    public MailNotification()
+    {
+    }
+    public string Title { get; set; }
+    public string Message { get; set; }
+}
+
+public class MailNotificationHandler : INotificationHandler<MailNotification>
+{
+    public Task Handle(MailNotification notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"{notification.Title} calisiyor. {notification.Message} ");
+        return Task.CompletedTask;
+    }
+}
